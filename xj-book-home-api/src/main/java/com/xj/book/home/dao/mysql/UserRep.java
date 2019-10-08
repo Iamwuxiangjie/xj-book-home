c081338f3f3a0e381b96ad5c0a068b721c13c4d6
WuXJ <992622784@qq.com>
2019-10-08 10:12:21 +0800

    wuxj

--- xj-book-home-api/src/main/java/com/xj/book/home/dao/mysql/UserRep.java
+++ xj-book-home-api/src/main/java/com/xj/book/home/dao/mysql/UserRep.java
@@ -5,13 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-/**
- * TODO
- *
- * @author: WuXJ
- * @date: 2019-08-18 18:38
- * Copyright (c) 2019, ewell.com All Rights Reserved.
- */
 @Repository
 public interface UserRep extends JpaRepository<User,Long>, JpaSpecificationExecutor<User> {
 
